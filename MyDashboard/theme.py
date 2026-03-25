37b93c4d561dce2decbd287cbd0d7a296f2f846c
syomin0309-create <syomin0309@gmail.com>
2026-03-25 16:46:13 +0800

    Update theme.py

--- MyDashboard/theme.py
+++ MyDashboard/theme.py
@@ -578,7 +578,7 @@ lottie.loadAnimation({{container:document.getElementById("card-cpk"),    rendere
 </body>
 </html>
 """
-    components.html(html, height=300, scrolling=False)
+    components.html(html, height=150, scrolling=False)
 
 
 # ── Plotly 圖表主題（CHART_THEME）────────────────────────
